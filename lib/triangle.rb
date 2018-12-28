d24befa1ca7382e4c83296b130eedd513fba0df0
Marie Burns <mcburns2222@gmail.com>
2018-12-28 21:10:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,7 +5,8 @@ class Triangle
   end
 
   def kind
-    if side1 ==
+    if (side1 + side2) < side3) || (side2 + side3 < side1) || (side1 + side3 < side2) || side1 <= 0 || side2 <= 0 || side3 <= 0 
+      
     
     if side1 == side2 && side1 == side3
       :equilateral
