97c4995bd90d32466cf47c82d5206d66201f1277
Marie Burns <mcburns2222@gmail.com>
2018-12-28 21:06:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,6 +7,8 @@ class Triangle
   def kind
     if side1 == side2 && side1 == side3
       :equilateral
+    elsif side1 == side2 || side1 == side2 || side2 == side3
+      :isosceles
     elsif 
   end
   
